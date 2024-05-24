d64a4d22e096d3deab40fa8e33427b06739b9ea5
unknown <miggy.payumo26@gmail.com>
2024-05-24 14:39:29 +0800

    Added definitions that are already tested in a different file

--- television_program.py
+++ television_program.py
@@ -10,7 +10,44 @@ class TV:
         self.volume = 1
         self.on = False
     
+    def turnOn(self):
+        self.on = True
     
+    def turnOff(self):
+        self.on = False
+    
+    def getChannel(self):
+        return self.channel
+    
+    def setChannel(self, channel):
+        if 1 <= channel <= 120:
+            self.channel = channel
+    
+    def getVolume(self):
+        return self.volume
+    
+    def setVolume(self, volume):
+        if 1 <= volume <= 7:
+            self.volume = volume
+    
+    def channelUp(self):
+        if self.channel < 120:
+            self.channel += 1
+    
+    def channelDown(self):
+        if self.channel > 1:
+            self.channel -= 1
+    
+    def volumeUp(self):
+        if self.volume < 7:
+            self.volume += 1
+    
+    def volumeDown(self):
+        if self.volume > 1:
+            self.volume -= 1
+
+
+
 
 
 
