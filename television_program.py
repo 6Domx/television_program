c5b9ffaf0c73eeb9e79cee3df04ce98d2c341cac
unknown <miggy.payumo26@gmail.com>
2024-05-24 14:21:28 +0800

    Class creation and variable naming

--- television_program.py
+++ television_program.py
@@ -3,4 +3,10 @@
 # 1. "tv1's channel is 30 and volume level is 3"
 # 2. "tv2's channel is 3 and volume level is 2"
 
+class TV:
+    def __init__(self, brand, channel=1, volume=50):
+        self.brand = brand
+        self.channel = channel
+        self.volume = volume
+        self.is_on = False
 
