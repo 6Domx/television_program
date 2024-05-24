46acfbc77347f4f64278522ffd46892eacb45e34
unknown <miggy.payumo26@gmail.com>
2024-05-24 14:38:50 +0800

    Variable rebuild for easier understanding

--- television_program.py
+++ television_program.py
@@ -3,10 +3,15 @@
 # 1. "tv1's channel is 30 and volume level is 3"
 # 2. "tv2's channel is 3 and volume level is 2"
 
+
 class TV:
-    def __init__(self, brand, channel=1, volume=50):
-        self.brand = brand
-        self.channel = channel
-        self.volume = volume
-        self.is_on = False
+    def __init__(self):
+        self.channel = 1
+        self.volume = 1
+        self.on = False
+    
+    
+
+
+
 
